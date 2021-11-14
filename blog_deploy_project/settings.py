40c71b6cf0a941e7ca99c112e22d35aef3f8568e
Slimjim821 <jim.n.johnson16@gmail.com>
2021-11-14 16:44:30 -0600

    Updated secret key

--- blog_deploy_project/settings.py
+++ blog_deploy_project/settings.py
@@ -27,7 +27,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+#SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = '9$vw346tn^f78o7fi@yj8oco_x$*%4ew6$wicw3^$9hhbb$#-t'
 
 DEBUG = True
 
